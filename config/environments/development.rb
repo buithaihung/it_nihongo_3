ada5c173357055adf447c2b442ea520f105be0e7
buithaihung <buithaihung6934@gmail.com>
2022-04-21 21:24:50 +0700

    fix code

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -8,11 +8,6 @@ Rails.application.configure do
 
   # Do not eager load code on boot.
   config.eager_load = false
-  config.eager_load = false
-  config.eager_load = false
-  config.eager_load = false
-  config.eager_load = false
-  //abcfasjdgajksdjkaskjjk
 
   # Show full error reports.
   config.consider_all_requests_local = true
